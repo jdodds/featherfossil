2ea940272d4879b570396a548ec584fce9632b90
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-02-27 18:35:34 -0500

    docstrings for Dispatcher

--- feather/dispatcher.py
+++ feather/dispatcher.py
@@ -2,12 +2,22 @@ from collections import defaultdict
 from multiprocessing import Queue
 
 class Dispatcher(object):
+    """Recieve messages from Plugins and send them to the Plugins that listen
+    for them.
+    """
     def __init__(self):
+        """Create our set of listeners, communication Queue, and empty set of
+        plugins.
+        """
         self.listeners = defaultdict(set)
         self.plugins = set()
         self.messages = Queue()
 
     def register(self, plugin):
+        """Add the plugin to our set of listeners for each message that it
+        listens to, tell it to use our messages Queue for communication, and
+        start it up.
+        """
         for listener in plugin.listeners:
             self.listeners[listener].add(plugin)
         self.plugins.add(plugin)
@@ -15,6 +25,10 @@ class Dispatcher(object):
         plugin.start()
 
     def start(self):
+        """Send 'APP_START' to any plugins that listen for it, and loop around
+        waiting for messages and sending them to their listening plugins until
+        it's time to shutdown.
+        """
         self.recieve('APP_START')
         self.alive = True
         while self.alive:
